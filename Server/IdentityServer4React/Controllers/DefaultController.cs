6d300bd3753e4c7077534e5665c04813f5e18d31
devdigital <devdigital.cloud@gmail.com>
2018-04-12 14:58:06 +0100

    chore: add index.html fallback

---
+++ Server/IdentityServer4React/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿namespace IdentityServer4React.Controllers
+{
+    using System;
+    using System.IO;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Net.Http.Headers;
+
+    public class DefaultController : Controller
+    {
+        private readonly IHostingEnvironment environment;
+
+        public DefaultController(IHostingEnvironment environment)
+        {
+            this.environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return new PhysicalFileResult(
+                Path.Combine(this.environment.WebRootPath, "index.html"),
+                new MediaTypeHeaderValue("text/html")
+            );
+        }
+    }
+}
--- Server/IdentityServer4React/Startup.cs
+++ Server/IdentityServer4React/Startup.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace IdentityServer4React.Api
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using Microsoft.AspNetCore.Http;
+
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
@@ -35,8 +39,36 @@ namespace IdentityServer4React.Api
                     .AllowAnyMethod()
             );
 
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                DefaultFileNames = new List<string> { "index.html" }
+            });
+
+            app.UseStaticFiles();
+
             app.UseIdentityServer();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                
+                routes.MapRoute(
+                    name: "catch-all",
+                    template: "{*url}",
+                    defaults: new { controller = "Default", action = "Index" }
+                );
+            });            
+
+            // Send index.html for non API requests
+            //app.MapWhen(
+            //    ctx => !ctx.Request.Path.Value.StartsWith("/api"),
+            //    b => b.Run(
+            //        async ctx =>
+            //        {
+            //            ctx.Response.ContentType = "text/html";
+            //            await ctx.Response.SendFileAsync(Path.Combine(env.WebRootPath, "index.html"));
+            //        }));
         }
     }
 }
