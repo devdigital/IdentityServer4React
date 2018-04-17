ca4268e659ca1eb56b775089a094eefec7636ada
devdigital <devdigital.cloud@gmail.com>
2018-04-17 16:14:23 +0100

    fix: sign in error event

--- Server/IdentityServer4React/Authentication/SignInApiModel.cs
+++ Server/IdentityServer4React/Authentication/SignInApiModel.cs
@@ -2,7 +2,7 @@
 {
     using System.ComponentModel.DataAnnotations;
 
-    public class AuthenticationApiModel
+    public class SignInApiModel
     {
         [Required]
         public string Username { get; set; }
